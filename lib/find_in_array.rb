e88bcbeee1ce71ee122f9d5ecc266c2f364ae341
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-05-18 19:00:31 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,11 +1,10 @@
 def find_element_index(array, value_to_find)
 count = 0
-found_value_index = nil
+
 while count < array.length do
-  puts array[count] == value_to_find
-    found_value_index = count
+  if array[count] == value_to_find
+    return count 
   end
   count += 1
-end
-found_value_index
+  end
 end
